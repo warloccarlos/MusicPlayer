65ba120d0078590c8d759c5c1c128b4e397b0b81
Warloc Carlos <warloccarlos@gmail.com>
2025-10-02 02:05:16 +0530

    Flask based MusicPlayer

--- player/player.py
+++ player/player.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template, request, redirect, url_for
 import os
+from flask import Blueprint, render_template
 
 player = Blueprint('player', __name__)
+
+directory = "static/audio"
 
 @player.route('/', methods=['GET', 'POST'])
 def index():
     path = os.path.dirname(os.path.abspath(__file__))
-    directory = 'static/audio'
     with os.scandir(os.path.join(path, directory)) as entries:
         audio_files = [entry.name for entry in entries if entry.is_file() and entry.name.endswith('.mp3')]
     return render_template('index.html', audio_files=audio_files, src=directory)
